0b070b789d12418fab4ee4fc9978537da097b58c
michallbujak <zmichal@outlook.com>
2022-04-06 11:07:02 +0200

    Update ExMAS_wrapper.py

--- Scripts/ExMAS_wrapper.py
+++ Scripts/ExMAS_wrapper.py
@@ -5,13 +5,13 @@ import osmnx as ox
 
 os.chdir(os.path.dirname(os.getcwd()))
 
-from ExMAS.main import main
+from ExMAS.main_prob import main
 from ExMAS.utils import get_config
 from ExMAS.utils import load_G
 import ExMAS.utils_nyc as utils_nyc
 from ExMAS.utils import inData as inData
 
-params = utils_nyc.get_config('ExMAS/data/configs/nyc.json')
+params = utils_nyc.get_config('ExMAS/data/configs/nyc_prob.json')
 inData = load_G(inData, params, stats = True)
 inData = utils_nyc.load_nyc_csv(inData, params)
 inData = utils_nyc.load_G(inData, params)
