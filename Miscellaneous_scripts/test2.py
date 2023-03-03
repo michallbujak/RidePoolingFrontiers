f28014b749faa80c5dff8f7261c0b7f0191ffa31
michallbujak <zmichal@outlook.com>
2023-03-03 15:14:28 +0100

    Update

--- Miscellaneous_scripts/test2.py
+++ Miscellaneous_scripts/test2.py
@@ -13,19 +13,8 @@ import seaborn as sns
 import scienceplots
 from collections import Counter
 
-plt.style.use(['science', 'no-latex'])
+with open(r"C:\Users\szmat\Documents\GitHub\ExMAS_sideline\Topology\data\results\25-11-22_baseline\dotmap_list_25-11-22.obj", 'rb') as file:
+    all_graphs = pickle.load(file)
 
-df = pd.read_excel(r"C:\Users\szmat\Documents\GitHub\ExMAS_sideline\Topology\data\results\19-01-23_full\frame_evolution_19-01-23.xlsx")
 
-plt, ax1 = plt.subplots(figsize=(4, 3), dpi=200)
-ax2 = ax1.twinx()
-# ax1.plot(df["average_degree"], label="Average degree", color='blue', lw=.5, marker='o', linestyle='solid', markersize=1)
-# ax2.plot(df["max_comp"], label="Greatest component", color='red', lw=.5, marker='o', linestyle='solid', markersize=1)
-ax1.plot(df["average_degree"], label="Average degree", color='blue', lw=1)
-ax2.plot(df["max_comp"], label="Largest component", color='red', lw=1)
-ax1.spines['left'].set_color('blue')
-ax2.spines['right'].set_color('red')
-ax1.tick_params(axis='y', colors='blue', which="both")
-ax2.tick_params(axis='y', colors='red', which="both")
-plt.legend(loc=(0.5, 0.2), fontsize=7)
-plt.savefig(r"C:\Users\szmat\Documents\GitHub\ExMAS_sideline\Topology\data\results\19-01-23_full\temp.png")
+e = 0
