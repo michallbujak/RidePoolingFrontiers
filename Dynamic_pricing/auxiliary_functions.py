5b2a8f78bf653db5e782eb1ec8b6965f17fd9b94
michal <zmichal@outlook.com>
2024-11-27 16:47:37 +0100

    Further development of dynamic pricing functions

--- Dynamic_pricing/auxiliary_functions.py
+++ Dynamic_pricing/auxiliary_functions.py
@@ -1,13 +1,12 @@
 """ Function to support the dynamic pricing algorithm """
 import secrets
 from typing import Callable
+import itertools
+from bisect import bisect_right
 
 import numpy as np
 import pandas as pd
-import tqdm
-
-from Individual_pricing.posteriori_analysis_old import price
-from Individual_pricing.pricing_functions import row_maximise_profit
+from tqdm import tqdm
 
 
 def prepare_samples(
@@ -79,7 +78,7 @@ def row_acceptable_discount_bayes(
         pax_out -= _times_non_shared[trav]
         pax_out = [(t[0] * pax_out, t[1]) for t in _bt_sample]
         pax_out = [(t[0], t[1]) if t[0] >= 0 else (0, t[1]) for t in pax_out]
-        pax_out /= (_fare * _rides_row["individual_distances"][no])
+        pax_out = [(t[0]/(_fare * _rides_row["individual_distances"][no]), t[1]) for t in pax_out]
         out.append(pax_out)
 
     return out
@@ -87,7 +86,9 @@ def row_acceptable_discount_bayes(
 
 def row_maximise_profit_bayes(
         _rides_row: pd.Series,
+        _class_membership: dict[dict],
         _max_output_func: Callable[[list], float],
+        _sample_size: int,
         _price: float = 0.0015,
         _probability_single: float = 1,
         _guaranteed_discount: float = 0.05,
@@ -98,6 +99,8 @@ def row_maximise_profit_bayes(
      of a precalculated exmas ride
     --------
     @param _rides_row: row of exmas rides (potential combination + characteristics)
+    @param _class_membership: probability for each agent to belong to a given class
+    @param _sample_size: number of samples of value of time for a class
     @param _price: per-kilometre fare
     @param _probability_single: probability that a customer is satisfied with a private
     ride if offered one
@@ -126,78 +129,62 @@ def row_maximise_profit_bayes(
         out += [_max_output_func(out)]
         return out
 
+    travellers = _rides_row["indexes"]
     discounts = _rides_row["accepted_discount"].copy()
-    discounts = [[_guaranteed_discount] + t if t[0] > _guaranteed_discount else t for t in discounts]
-    discounts = list(itertools.product(*discounts))
-
-    # discounts = list(itertools.product(*_rides_row["accepted_discount"]))
-    discounts = [[t if t > _guaranteed_discount else _guaranteed_discount
-                  for t in discount] for discount in discounts]
-    discounts = list(set(tuple(discount) for discount in discounts))
+    discounts = [t if t[0] > _guaranteed_discount else (_guaranteed_discount, t[1])
+                 for t in discounts] # at least guaranteed discount
+    discounts = list(itertools.product(*discounts)) # create a list of bi-vectors (disc, class) of discounts (everyone gets one)
     base_revenues = {num: _rides_row["individual_distances"][num] * _price for num, t in
                      enumerate(_rides_row['indexes'])}
     best = [0, 0, 0, 0, 0, 0]
-    # if _rides_row['indexes'] == [142, 133]:
-    #     print('ee')
+
     for discount in discounts:
-        """ For effectively shared ride """
-        eff_price = [_price * (1 - t) for t in discount]
+        # Start with the effectively shared ride
+        eff_price = [_price * (1 - t[0]) for t in discount]
         revenue_shared = [a * b for a, b in
                           zip(_rides_row["individual_distances"], eff_price)]
         probability_shared = 1
-        prob_ind = [0] * len(discount)
+        prob_individual = [.0] * len(discount)
 
-        # if _rides_row['indexes'] == [20, 39, 53]:
-        #     print('eyy')
-
-        for num, individual_disc in enumerate(discount):
+        for num, individual_disc, pax in enumerate(zip(discount, travellers)):
             accepted_disc = _rides_row["accepted_discount"][num]
-            prob_ind[num] = bisect_right(accepted_disc, individual_disc) / len(accepted_disc)
-            probability_shared *= prob_ind[num]
+            prob_individual[num] = sum(0 if t[0] > individual_disc else _class_membership[pax][t[1]]
+                                       for t in accepted_disc)/_sample_size
+            probability_shared *= prob_individual[num]
 
         if probability_shared < _min_acceptance:
             continue
 
-        if _one_shot:
-            # out = [sum(revenue) * probability - cost, discount, sum(revenue), probability, cost]
-            out = [sum(revenue_shared) * probability_shared,
-                   discount,
-                   sum(revenue_shared),
-                   prob_ind,
-                   _rides_row["veh_dist"] * probability_shared / 1000]
-            max_out = _max_output_func(out)
-            out += [max_out]
-            if max_out > best[-1]:
-                best = out.copy()
-        else:
-            remaining_revenue = 0
-            for num_trav in range(len(discount)):
-                # First, if the P(X_j = 0)*r_j
-                prob_not_trav = 1 - prob_ind[num_trav]
-                remaining_revenue += prob_not_trav * base_revenues[num_trav]
-                # Then, P(X_j = 1, \pi X_i = 0)*r_j*(1-\lambda)
-                others_not = 1 - np.prod([t for num, t in enumerate(prob_ind) if num != num_trav])
-                rev_discounted = base_revenues[num_trav] * (1 - _guaranteed_discount)
-                remaining_revenue += (prob_ind[num_trav] * others_not) * rev_discounted
+        remaining_revenue = 0
+        for num_trav in range(len(discount)):
+            # First, if the P(X_j = 0)*r_j
+            prob_not_trav = 1 - prob_individual[num_trav]
+            remaining_revenue += prob_not_trav * base_revenues[num_trav]
+            # Then, P(X_j = 1, \pi X_i = 0)*r_j*(1-\lambda)
+            others_not = 1 - np.prod([t for num, t in enumerate(prob_individual) if num != num_trav])
+            rev_discounted = base_revenues[num_trav] * (1 - _guaranteed_discount)
+            remaining_revenue += (prob_individual[num_trav] * others_not) * rev_discounted
 
-            out = [sum(revenue_shared) * probability_shared + remaining_revenue,
-                   discount,
-                   sum(revenue_shared),
-                   prob_ind,
-                   (_rides_row["veh_dist"] * probability_shared +
-                    sum(_rides_row["individual_distances"]) * (1 - probability_shared)) / 1000]
-            max_out = _max_output_func(out)
-            out += [max_out]
+        out = [sum(revenue_shared) * probability_shared + remaining_revenue,
+               [t[0] for t in discount],
+               sum(revenue_shared),
+               prob_individual,
+               (_rides_row["veh_dist"] * probability_shared +
+                sum(_rides_row["individual_distances"]) * (1 - probability_shared)) / 1000]
+        max_out = _max_output_func(out)
+        out += [max_out]
 
-            if max_out > best[-1]:
-                best = out.copy()
+        if max_out > best[-1]:
+            best = out.copy()
 
     return best
 
 
-def expected_profit_function(
+def optimise_discounts(
         rides: pd.DataFrame,
         requests: pd.DataFrame,
+        class_membership: dict,
+        times_ns: dict,
         bt_sample: np.array,
         bs_levels: list[float] or dict,
         objective_func: Callable[[list], float] = lambda x: x[0] - x[4],
@@ -207,22 +194,23 @@ def expected_profit_function(
         speed: float = 6
 ) -> pd.DataFrame:
 
-    times_non_shared = dict(requests['ttrav'])
-    tqdm.pandas()
+    tqdm.pandas(desc="Accepted discount calculations")
 
     rides["accepted_discount"] = rides.progress_apply(
         row_acceptable_discount_bayes,
         axis=1,
-        _times_non_shared=times_non_shared,
-        _bs_samples=bs_levels,
+        _times_non_shared=times_ns,
+        _class_membership=class_membership,
+        _bs_levels=bs_levels,
         _bt_sample=bt_sample,
         _interval=1,
-        _price=fare
+        _fare=fare
     )
 
     rides["veh_dist"] = rides["u_veh"] * speed
 
-    rides["best_profit"] = rides.progress_apply(row_maximise_profit,
+    tqdm.pandas(desc="Discount optimisation")
+    rides["best_profit"] = rides.progress_apply(row_maximise_profit_bayes,
                                                 axis=1,
                                                 _price=fare,
                                                 _one_shot=False,
--- Dynamic_pricing/main_call.py
+++ Dynamic_pricing/main_call.py
@@ -1,6 +1,7 @@
 """ Wrapper for dynamic pricing algorithm """
 import argparse
 import ast
+import json
 import random
 import secrets
 
@@ -11,17 +12,10 @@ import Individual_pricing.pricing_utils.batch_preparation as batch_prep
 from Individual_pricing.pricing_functions import expand_rides
 from Individual_pricing.exmas_loop import exmas_loop_func
 from ExMAS.probabilistic_exmas import main as exmas_algo
-from Dynamic_pricing.auxiliary_functions import prepare_samples
+from Dynamic_pricing.auxiliary_functions import prepare_samples, optimise_discounts
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--directories-json", type=str, required=True)
-parser.add_argument("--profitability", action="store_true")
-parser.add_argument("--days", type=int, default=5)
-parser.add_argument("--daily-users", type=int, default=100)
-parser.add_argument("--min-accept", type=float, default=0.1)
-parser.add_argument("--operating-cost", type=float, default=1)
-parser.add_argument("--batch-size", type=int, default=150)
-parser.add_argument("--sample-size", type=int, default=5)
 parser.add_argument("--save-partial", action="store_false")
 parser.add_argument("--starting-step", type=int, default=0)
 parser.add_argument("--simulation-name", type=str or None, default=None)
@@ -30,11 +24,11 @@ args = parser.parse_args()
 print(args)
 
 # Import configuration & prepare results folder
-directories = batch_prep.get_parameters(args.directories_json)
+run_config = batch_prep.get_parameters(args.directories_json)
 batch_prep.create_results_directory(
-    directories,
-    str(args.batch_size) + "_" + str(args.sample_size),
-    new_directories=True,
+    run_config,
+    str(run_config.batch_size) + "_" + str(run_config.sample_size),
+    new_directories=False,
     directories_path=args.directories_json
 )
 
@@ -45,21 +39,21 @@ compute_save = [args.starting_step == 0, 0, args.starting_step, args.save_partia
 
 # Step PP1: Prepare behavioural samples (variable: value of time
 if compute_save[0]:
-    agents_class_prob = {j: [1/2, 1/2] for j in range(args.batch_size)}
+    agents_class_prob = {j: [1/2, 1/2] for j in range(run_config.batch_size)}
     bt_sample = prepare_samples(
-        sample_size=args.sample_size,
-        means=directories.means,
-        st_devs=directories.st_devs,
+        sample_size=run_config.sample_size,
+        means=run_config.means,
+        st_devs=run_config.st_devs,
         seed=args.seed
     )
 
 # Step PP2: Obtain demand
 if compute_save[0]:
     demand, exmas_params = batch_prep.prepare_batches(
-        exmas_params=batch_prep.get_parameters(directories.initial_parameters),
-        filter_function=lambda x: len(x.requests) == args.batch_size,
+        exmas_params=batch_prep.get_parameters(run_config.initial_parameters),
+        filter_function=lambda x: len(x.all_requests) == run_config.batch_size,
         quick_load=True,
-        batch_size=args.batch_size
+        batch_size=run_config.batch_size
     )
 
 # Step PP3: Create a dense shareability graph & data manipulation
@@ -73,43 +67,63 @@ if compute_save[0]:
 
 # Save data if requested
 if compute_save[0] & compute_save[3]:
-    batch_prep.create_directory(directories.partial_results + 'Step_0')
-    folder = directories.partial_results + '/Step_0/'
-    requests = demand['exmas']['requests']
-    rides = demand['exmas']['rides']
-    requests.to_csv(folder + 'demand_sample_' + '_' + str(args.batch_size) + '.csv',
-                    index=False)
-    rides.to_csv(folder + 'rides' + '_' + str(args.batch_size) + '.csv', index=False)
-    np.save(folder + 'sample' + '_' + str(args.sample_size), bt_sample)
+    batch_prep.create_directory(run_config.partial_results + 'Step_0')
+    folder = run_config.partial_results + 'Step_0/'
+    all_requests = demand['exmas']['requests']
+    all_rides = demand['exmas']['rides']
+    all_requests.to_csv(folder + 'demand_sample_' + str(run_config.batch_size) + '.csv',
+                        index=False)
+    all_rides.to_csv(folder + 'rides' + '_' + str(run_config.batch_size) + '.csv', index=False)
+    np.save(folder + 'sample' + '_' + str(run_config.sample_size), bt_sample)
+    with open(folder + 'exmas_config_' + str(run_config.sample_size) + '.json', 'w') as _file:
+        json.dump(exmas_params, _file)
 
 # Skip steps PP1-PP3 and load data
 if compute_save[2] - compute_save[1] == 1:
-    rides, requests, bt_sample = None, None, None
-    folder = directories.partial_results + 'Step_0/'
-    requests = pd.read_csv(folder + 'demand_sample_' + '_' + str(args.batch_size) + '.csv')
-    rides = pd.read_csv(folder + 'rides' + '_' + str(args.batch_size) + '.csv',
-                        converters={k: ast.literal_eval for k in
+    all_rides, all_requests, bt_sample = None, None, None
+    folder = run_config.partial_results + 'Step_0/'
+    all_requests = pd.read_csv(folder + 'demand_sample_' + str(run_config.batch_size) + '.csv')
+    all_rides = pd.read_csv(folder + 'rides' + '_' + str(run_config.batch_size) + '.csv',
+                            converters={k: ast.literal_eval for k in
                                     ['indexes', 'u_paxes', 'individual_times', 'individual_distances']})
-    bt_sample = np.load(folder + 'sample' + '_' + str(args.sample_size) + '.npy')
+    bt_sample = np.load(folder + 'sample' + '_' + str(run_config.sample_size) + '.npy')
+    with open(folder + 'exmas_config_' + str(run_config.sample_size) + '.json', 'r') as _file:
+        exmas_params = json.load(_file)
 
 compute_save[1] += 1
 compute_save[0] = compute_save[2] <= compute_save[1]
 
 """ Proceed to evolutionary part of the analysis """
 users_per_day = {}
-for day in range(args.days):
+class_membership_prob = {u: {_: v for _, v in enumerate(run_config['class_probs'])}
+                         for u in range(150)}
+times_non_shared = dict(all_requests['ttrav'])
+
+for day in range(run_config.no_days):
     # Step E1: filter the shareability graph for a users on a current day
     rng = np.random.default_rng(secrets.randbits(args.seed))
     random.seed(args.seed)
-    _ = int(rng.normal(args.daily_users, args.daily_users/100))
-    no_users = _ if (_ > 0 & _ <= args.daily_users) else args.batch_size
-    users = sorted(random.sample(range(150), no_users))
+    _ = int(rng.normal(run_config.daily_users, run_config.daily_users/100))
+    no_users = _ if (_ > 0 & _ <= run_config.daily_users) else run_config.batch_size
+    users = sorted(random.sample(range(run_config.batch_size), no_users))
     users_per_day[day] = users.copy()
-    rides_day = rides.loc[rides['indexes'].apply(
+    rides_day = all_rides.loc[all_rides['indexes'].apply(
         lambda _x: all(t in users for t in _x)
     )]
-    requests_day = requests.loc[requests['index'].apply(lambda _x: _x in users)]
+    requests_day = all_requests.loc[all_requests['index'].apply(lambda _x: _x in users)]
 
     # Step E2: Optimal pricing
-    rides_day = rides.copy()
+    rides_day = optimise_discounts(
+        rides=rides_day,
+        requests=requests_day,
+        class_membership=class_membership_prob,
+        times_ns=times_non_shared,
+        bt_sample=bt_sample,
+        bs_levels=[1, 1, 1.1, 1.2, 1.4, 2],
+        objective_func=lambda x: x[0] - run_config.mileage_sensitivity*x[4] - run_config.flat_fleet_cost,
+        min_acceptance=run_config.minimum_acceptance_probability,
+        guaranteed_discount=run_config.guaranteed_discount,
+        fare=exmas_params['price'],
+        speed=exmas_params['avg_speed']
+    )
 
